d724121e6d2242dc30950e3f51439e14338b9a78
Lehsey <57093112+Lehsey@users.noreply.github.com>
2022-05-05 22:05:27 +0300

    LOGIX_TOTAL_FIX

--- WinFormsApp1/2048_Logick.cs
+++ WinFormsApp1/2048_Logick.cs
@@ -118,28 +118,108 @@ namespace WinFormsApp1
             return true;
         }
 
-        private int find_val_in_col(int col, int start)
+        private int find_val_in_line(int line, int start, bool reverse)
         {
-            for (int i = start; i < Height; i++)
+            if (reverse)
             {
-                if (Bloks[i, col] != 0)
+                for (int i = start; i >= 0; i--)
                 {
-                    return i;
+                    if (Bloks[line, i] != 0)
+                    {
+                        return i;
+                    }
                 }
+                return -1;
             }
-            return -1;
+            else
+            {
+                for (int i = start; i < Width; i++)
+                {
+                    if (Bloks[line, i] != 0)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
         }
 
-        private int find_zero_in_col(int col, int start)
+        private int find_zero_in_line(int line, int start, bool reverse)
         {
-            for (int i = start; i < Height; i++)
+            if (reverse)
             {
-                if (Bloks[i, col] == 0)
+                for (int i = start; i >= 0; i--)
                 {
-                    return i;
+                    if (Bloks[line, i] == 0)
+                    {
+                        return i;
+                    }
                 }
+                return -1;
             }
-            return -1;
+            else
+            {
+                for (int i = start; i < Width; i++)
+                {
+                    if (Bloks[line, i] == 0)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
+        private int find_val_in_col(int col, int start, bool reverse)
+        {
+            if (reverse)
+            {
+                for (int i = start; i >= 0; i--)
+                {
+                    if (Bloks[i, col] != 0)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            else
+            {
+                for (int i = start; i < Height; i++)
+                {
+                    if (Bloks[i, col] != 0)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+        }
+
+        private int find_zero_in_col(int col, int start, bool reverse)
+        {
+            if (reverse)
+            {
+                for (int i = start; i >= 0; i--)
+                {
+                    if (Bloks[i, col] == 0)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
+            else
+            {
+
+                for (int i = start; i < Height; i++)
+                {
+                    if (Bloks[i, col] == 0)
+                    {
+                        return i;
+                    }
+                }
+                return -1;
+            }
         }
 
         private void move_up()
@@ -148,7 +228,7 @@ namespace WinFormsApp1
             int last_free_space;
             for (int j = 0; j < Width; j++)
             {
-                old_pos = find_val_in_col(j, 0);
+                old_pos = find_val_in_col(j, 0, false);
                 for (int i = 0; i < Height; i++)
                 {
                     if (old_pos == -1)
@@ -166,7 +246,7 @@ namespace WinFormsApp1
                         Bloks[old_pos, j] *= 2;
                         Session_score += Bloks[old_pos, j];
                         Bloks[i, j] = 0;
-                        old_pos = find_val_in_col(j, i);
+                        old_pos = find_val_in_col(j, i, false);
                     }
                     else if ((Bloks[i, j] != Bloks[old_pos, j]) && (Bloks[i, j] != 0))
                     {
@@ -177,7 +257,7 @@ namespace WinFormsApp1
 
             for (int j = 0; j < Width; j++)
             {
-                last_free_space = find_zero_in_col(j, 0);
+                last_free_space = find_zero_in_col(j, 0, false);
                 for(int i = 0; i < Height; i++)
                 {
                     if (last_free_space == -1)
@@ -189,7 +269,7 @@ namespace WinFormsApp1
                     {
                         Bloks[last_free_space, j] = Bloks[i, j];
                         Bloks[i, j] = 0;
-                        last_free_space = find_zero_in_col(j, last_free_space);
+                        last_free_space = find_zero_in_col(j, last_free_space, false);
                     }
                 }
             }
@@ -198,18 +278,161 @@ namespace WinFormsApp1
 
         private void move_down()
         {
-            
+            int old_pos;
+            int last_free_space;
+            for (int j = 0; j < Width; j++)
+            {
+                old_pos = find_val_in_col(j, Height - 1, true);
+                for (int i = Height - 1; i >= 0; i--)
+                {
+                    if (old_pos == -1)
+                    {
+                        break;
+                    }
+                    if ((i == old_pos) | (i > old_pos))
+                    {
+                        continue;
+                    }
+                    if (Bloks[i, j] == Bloks[old_pos, j])
+                    {
+                        Bloks[old_pos, j] *= 2;
+                        Session_score += Bloks[old_pos, j];
+                        Bloks[i, j] = 0;
+                        old_pos = find_val_in_col(j, i, true);
+                    }
+                    else if ((Bloks[i, j] != Bloks[old_pos, j]) && (Bloks[i, j] != 0))
+                    {
+                        old_pos = i;
+                    }
+                }
+            }
+
+            for (int j = 0; j < Width; j++)
+            {
+                last_free_space = find_zero_in_col(j, Height - 1, true);
+                for (int i = Height - 1; i >= 0; i--)
+                {
+                    if (last_free_space == -1)
+                    {
+                        break;
+                    }
+
+                    if ((Bloks[i, j] != 0) && (last_free_space > i))
+                    {
+                        Bloks[last_free_space, j] = Bloks[i, j];
+                        Bloks[i, j] = 0;
+                        last_free_space = find_zero_in_col(j, last_free_space, true);
+                    }
+                }
+            }
+
         }
 
         private void move_left()
         {
-            
+            int old_pos;
+            int last_free_space;
+            for (int i = 0; i < Height; i++)
+            {
+                old_pos = find_val_in_line(i, 0, false);
+                for (int j = 0; j < Width; j++)
+                {
+                    if (old_pos == -1)
+                    {
+                        break;
+                    }
+
+                    if ((j == old_pos) | (j < old_pos))
+                    {
+                        continue;
+                    }
+
+                    if (Bloks[i, j] == Bloks[i, old_pos])
+                    {
+                        Bloks[i, old_pos] *= 2;
+                        Session_score += Bloks[i, old_pos];
+                        Bloks[i, j] = 0;
+                        old_pos = find_val_in_line(i, j, false);
+                    }
+                    else if ((Bloks[i, j] != Bloks[i, old_pos]) && (Bloks[i, j] != 0))
+                    {
+                        old_pos = j;
+                    }
+                }
+            }
+
+            for (int i = 0; i < Height; i++)
+            {
+                last_free_space = find_zero_in_line(i, 0, false);
+                for (int j = 0; j < Width; j++)
+                {
+                    if (last_free_space == -1)
+                    {
+                        break;
+                    }
+
+                    if ((Bloks[i, j] != 0) && (last_free_space < j))
+                    {
+                        Bloks[i, last_free_space] = Bloks[i, j];
+                        Bloks[i, j] = 0;
+                        last_free_space = find_zero_in_line(i, last_free_space, false);
+                    }
+                }
+            }
         }
 
         private void move_right()
         {
-          
-        
+            int old_pos;
+            int last_free_space;
+            for (int i = 0; i < Height; i++)
+            {
+                old_pos = find_val_in_line(i, Width - 1, true);
+                for (int j = Width - 1; j >= 0; j--)
+                {
+                    if (old_pos == -1)
+                    {
+                        break;
+                    }
+
+                    if ((j == old_pos) | (j > old_pos))
+                    {
+                        continue;
+                    }
+
+                    if (Bloks[i, j] == Bloks[i, old_pos])
+                    {
+                        Bloks[i, old_pos] *= 2;
+                        Session_score += Bloks[i, old_pos];
+                        Bloks[i, j] = 0;
+                        old_pos = find_val_in_line(i, j, true);
+                    }
+                    else if ((Bloks[i, j] != Bloks[i, old_pos]) && (Bloks[i, j] != 0))
+                    {
+                        old_pos = j;
+                    }
+                }
+            }
+
+            for (int i = 0; i < Height; i++)
+            {
+                last_free_space = find_zero_in_line(i, Width - 1, true);
+                for (int j = Width - 1; j >= 0; j--)
+                {
+                    if (last_free_space == -1)
+                    {
+                        break;
+                    }
+
+                    if ((Bloks[i, j] != 0) && (last_free_space > j))
+                    {
+                        Bloks[i, last_free_space] = Bloks[i, j];
+                        Bloks[i, j] = 0;
+                        last_free_space = find_zero_in_line(i, last_free_space, true);
+                    }
+                }
+            }
+
         }
     }
 }
