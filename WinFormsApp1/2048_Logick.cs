a2643de10f326e885afe8f59aa75f116cc762d1c
Lehsey <57093112+Lehsey@users.noreply.github.com>
2022-05-15 18:34:08 +0300

    logick comments

--- WinFormsApp1/2048_Logick.cs
+++ WinFormsApp1/2048_Logick.cs
@@ -6,20 +6,22 @@ using System.Threading.Tasks;
 
 namespace WinFormsApp1
 {
+    // класс логики
     public class Logick
     {
         
-        private int[,] Bloks;
-        private int Width, Height;
-        public int Common_value_gen = 2;
-        private int Rare_value_gen = 4;
-        public int Session_score = 0;
+        private int[,] Bloks; // массив значений таблицы
+        private int Width, Height; // ширина и высота таблицы
+        public int Common_value_gen = 2; // 90 %значение нового бока 
+        private int Rare_value_gen = 4; // 10: значение нового блока
+        public int Session_score = 0; // счёт текущей сессии
 
 
         public Logick(int _width, int _height)
         {
             Width = _width;
             Height = _height;
+            // создание и заполнение догического массива
             Bloks = new int[Height, Width];
             for (int i = 0; i < Height; i++)
             {
@@ -28,24 +30,29 @@ namespace WinFormsApp1
                     Bloks[i, j] = 0;
                 }
             }
+            // генерация 2х начальных блоков
             Generate_new_blok();
             Generate_new_blok();
         }
 
+        // возврат массива значений для передачи в граф. оболочку
         public int[,] Get_Bloks()
         {
             return Bloks;
         }
 
+        // возврат счёта сессии для передачи в граф. оболочку
         public int GetScore()
         {
             return Session_score;
         }
 
+        // метод генерации нового блока после хода
         private void Generate_new_blok()
         {
             System.Random rnd = new System.Random();
             int i, j;
+            // выбор координат "нового" не нулевого блока
             i = rnd.Next(0, Height);
             j = rnd.Next(0, Width);
             while (Bloks[i, j] != 0)
@@ -53,7 +60,7 @@ namespace WinFormsApp1
                 i = rnd.Next(0, Height);
                 j = rnd.Next(0, Width);
             }
-
+            // присваивание нового значения нуджному блоку
             if (rnd.Next(0, 10) == 1)
             {
                 Bloks[i,j] = Rare_value_gen;
@@ -65,6 +72,7 @@ namespace WinFormsApp1
 
         }
 
+        // метод вызова нужного сдвина и последующей генерации нового значения если требуется
         public void try_move(bool Up = false, bool Down = false, bool Left = false, bool Right = false)
         {
             if (Up)
@@ -90,6 +98,7 @@ namespace WinFormsApp1
             }
         }
 
+        // метод проверки на пустое (нулевое) значение в массиве
         private bool Free_space()
         {
             for (int i = 0; i < Height; i++)
@@ -105,8 +114,10 @@ namespace WinFormsApp1
             return false;
         }
 
+        // метод проверки можно ли ещё сделать ход
         public bool Game_over()
         {
+            // проверка вохможных ходов всех тайлов кроме крайних правых и нижних
             for (int i = 0; i < Height - 1; i++)
             {
                 for (int j = 0; j < Width - 1; j++)
@@ -117,6 +128,7 @@ namespace WinFormsApp1
                     }
                 }
             }
+            // проверка возможжности ходов крайних правых тайлов
             for (int i = 0; i < Height - 1; i++)
             {
                 if (Bloks[i, Width - 1] == Bloks[i + 1, Width - 1])
@@ -125,6 +137,7 @@ namespace WinFormsApp1
                 }
             }
 
+            //проверка возможности ходов крайних нижних тайлов
             for (int i = 0; i < Width - 1; i++)
             {
                 if (Bloks[Height - 1, i] == Bloks[Height - 1, i + 1])
@@ -135,8 +148,10 @@ namespace WinFormsApp1
             return true;
         }
 
+        //метод поиск не нулевого значния в строчке
         private int find_val_in_line(int line, int start, bool reverse)
         {
+            //поиск значения в порядке справа на лево
             if (reverse)
             {
                 for (int i = start; i >= 0; i--)
@@ -146,9 +161,10 @@ namespace WinFormsApp1
                         return i;
                     }
                 }
-                return -1;
+                return -1; //все значения нулевые
             }
             else
+            // в порядке слева на право
             {
                 for (int i = start; i < Width; i++)
                 {
@@ -157,12 +173,14 @@ namespace WinFormsApp1
                         return i;
                     }
                 }
-                return -1;
+                return -1; // все значения нулевые
             }
         }
 
+        // метод поиск нулевого значения в строке
         private int find_zero_in_line(int line, int start, bool reverse)
         {
+            // в порядке справа на лево
             if (reverse)
             {
                 for (int i = start; i >= 0; i--)
@@ -172,9 +190,10 @@ namespace WinFormsApp1
                         return i;
                     }
                 }
-                return -1;
+                return -1; // все начения не равны 0
             }
             else
+            // в порядке слева на право
             {
                 for (int i = start; i < Width; i++)
                 {
@@ -183,11 +202,14 @@ namespace WinFormsApp1
                         return i;
                     }
                 }
-                return -1;
+                return -1; // все значения не равны 0
             }
         }
+
+        // поиск не нулевого значения в колонке
         private int find_val_in_col(int col, int start, bool reverse)
         {
+            // снизу верх
             if (reverse)
             {
                 for (int i = start; i >= 0; i--)
@@ -200,6 +222,7 @@ namespace WinFormsApp1
                 return -1;
             }
             else
+            // сверху вниз
             {
                 for (int i = start; i < Height; i++)
                 {
@@ -212,9 +235,11 @@ namespace WinFormsApp1
             }
         }
 
+        // поиск нулевого значения в колонке
         private int find_zero_in_col(int col, int start, bool reverse)
         {
             if (reverse)
+            // снизу вверх
             {
                 for (int i = start; i >= 0; i--)
                 {
@@ -226,6 +251,7 @@ namespace WinFormsApp1
                 return -1;
             }
             else
+            //сверху вниз
             {
 
                 for (int i = start; i < Height; i++)
@@ -239,20 +265,22 @@ namespace WinFormsApp1
             }
         }
 
+        // метод сдвига вверх
         private void move_up()
         {
-            int old_pos;
-            int last_free_space;
+            int old_pos; // позиция тайла текущего отсчёта
+            int last_free_space; // позиция тайла ближайшего нулевого тайла относительно движения
             for (int j = 0; j < Width; j++)
             {
-                old_pos = find_val_in_col(j, 0, false);
+                old_pos = find_val_in_col(j, 0, false); // поиск позиции ненулевого тайла в текущей колонке
                 for (int i = 0; i < Height; i++)
                 {
+                    // если такого нет - пропускаем колонку
                     if (old_pos == -1)
                     {
                         break;
                     }
-
+                    // если координаты текущего тайла меньше или равны последнего ненулевого тайла, пропускаем 1 итерацию
                     if ((i == old_pos) | (i < old_pos))
                     {
                         continue;
@@ -260,39 +288,42 @@ namespace WinFormsApp1
 
                     if (Bloks[i, j] == Bloks[old_pos, j])
                     {
-                        Bloks[old_pos, j] *= 2;
+                        Bloks[old_pos, j] *= 2; // если текущий тайл равен последнему ненулевому тайлу то последний не нулевой тайл удваивается, а текущий обнуляется
                         Session_score += Bloks[old_pos, j];
                         Bloks[i, j] = 0;
-                        old_pos = find_val_in_col(j, i, false);
+                        old_pos = find_val_in_col(j, i, false); // обновление позиции последнего ненулевого тайла, поиск начинается с текущей позиции
                     }
-                    else if ((Bloks[i, j] != Bloks[old_pos, j]) && (Bloks[i, j] != 0))
+                    else if ((Bloks[i, j] != Bloks[old_pos, j]) && (Bloks[i, j] != 0)) // если текущий тайл не равен 0 и не равне последнему не нулевому, текущий тайл становится последним не нулевым
                     {
                         old_pos = i;
                     }
                 }
             }
 
+            // Сдвиг колонки в нужную сторону в зависимости от стороны сдвига
+
             for (int j = 0; j < Width; j++)
             {
-                last_free_space = find_zero_in_col(j, 0, false);
+                last_free_space = find_zero_in_col(j, 0, false); // нахождение первой нулевой позхиции тайла 
                 for(int i = 0; i < Height; i++)
                 {
                     if (last_free_space == -1)
                     {
-                        break;
+                        break; // если все тайлы не равны 0, то прерываем проверку
                     }
 
-                    if ((Bloks[i, j] != 0) && (last_free_space < i))
+                    if ((Bloks[i, j] != 0) && (last_free_space < i)) // если текущий тайл не равен нулю, то он переносится на позицию нулевого тайла
                     {
                         Bloks[last_free_space, j] = Bloks[i, j];
                         Bloks[i, j] = 0;
-                        last_free_space = find_zero_in_col(j, last_free_space, false);
+                        last_free_space = find_zero_in_col(j, last_free_space, false); // поиск нового ненулевого тайла
                     }
                 }
             }
 
         }
 
+        // сдвиг вниз - аналогично сдвигу вверх, но реверсированно
         private void move_down()
         {
             int old_pos;
@@ -344,7 +375,8 @@ namespace WinFormsApp1
             }
 
         }
-
+        
+        // Сдвиг в лево - аналогично свдигу вверх, но теперь обход идёт по строчкам а не колонкам
         private void move_left()
         {
             int old_pos;
@@ -398,6 +430,7 @@ namespace WinFormsApp1
             }
         }
 
+        // аналогично сдвигу влево, но реверсировано
         private void move_right()
         {
             int old_pos;
